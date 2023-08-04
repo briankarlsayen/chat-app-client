fee92cdd284d1662ac2affab7127f5785471b1b1
BrianSayen <blu3fire89@gmail.com>
2023-08-04 16:45:53 +0800

    update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,14 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:5100/api',
-  // baseURL: 'http://localhost:5081',
-  // baseURL: 'https://barker-server-bd483ayno-briankarlsayen.vercel.app',
-  // baseURL: "https://SmoggyFrighteningEnvironments.blu3fire89.repl.co",
-  // baseURL: "https://openai-server.onrender.com",
-  // baseURL: 'https://twitty-server-p6yjbz8no-briankarlsayen.vercel.app',
-  // baseURL: 'https://openai-server-eight.vercel.app',
-  baseURL: 'http://localhost:5900',
+  baseURL: 'https://chat-app-server-nu-henna.vercel.app',
+  // baseURL: 'http://localhost:5900',
 });
 
 export default instance;
