371e169a85ddd7ead4e4aa84577dea9de9169e3d
BrianSayen <blu3fire89@gmail.com>
2023-08-11 16:59:07 +0800

    update

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,2 +1,2 @@
-// export const url = 'https://chat-app-server.fly.dev';
-export const url = 'http://localhost:5900';
+export const url = 'https://chat-app-server.fly.dev';
+// export const url = 'http://localhost:5900';
