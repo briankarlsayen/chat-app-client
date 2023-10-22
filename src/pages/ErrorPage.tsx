f2baa5b88b4e14b6032dadfc0ed0a4205b81ab5a
briankarlsayen <blu3fire89@gmail.com>
2023-10-22 12:11:30 +0800

    update

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -7,7 +7,7 @@ function ErrorPage() {
     <div className='flex flex-col h-screen items-center justify-center'>
       <img src={ErrorImg} className='w-96 h-96' alt='error-img' />
       <p className='text-lg'>
-        Something went wrong. Please reload website to continue.
+        Something went wrong. Please reload website to continue. Error.
       </p>
       <button className='btn-blue mt-2' onClick={handleReload}>
         Reload
