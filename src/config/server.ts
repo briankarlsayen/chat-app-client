a181c50596da65774d5e17227c2c17885fe8d3c2
briankarlsayen <blu3fire89@gmail.com>
2023-10-22 11:50:53 +0800

    update

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,3 +1,3 @@
 // export const url = 'https://chat-app-server.fly.dev';
 export const apiUrl = process.env.NODE_ENV === 'production' ? 'api' : 'http://localhost:5900/api';
-export const socketUrl = process.env.NODE_ENV === 'production' ? '/api/chat-app' : 'http://localhost:5900/chat-app';
+export const socketUrl = process.env.NODE_ENV === 'production' ? 'api/chat-app' : 'http://localhost:5900/chat-app';
